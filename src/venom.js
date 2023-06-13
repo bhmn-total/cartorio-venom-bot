86918b1e4707c90dd95ece4ee21913d4f7b00417
Arthur Azevedo <arthuraz.1990@gmail.com>
2023-06-13 15:02:40 -0300

    Ajuste na exportação do qrcode

--- src/venom.js
+++ src/venom.js
@@ -9,13 +9,50 @@ export class VenomBot {
     return VenomBot.instance
   }
 
+  fnLogQrCode (base64Qrimg, asciiQR, attempts, urlCode) {
+    console.log('Number of attempts to read the qrcode: ', attempts);
+    console.log('Terminal qrcode: ', asciiQR);
+    console.log('base64 image string qrcode: ', base64Qrimg);
+    console.log('urlCode (data-ref): ', urlCode);
+  }
+
+  exportQrCode(base64Qr, asciiQR, attempts, urlCode) {
+    const matches = base64Qr.match(/^data:([A-Za-z-+\/]+);base64,(.+)$/),
+      response = {};
+
+    if (matches.length !== 3) {
+      return new Error('Invalid input string');
+    }
+    response.type = matches[1];
+    response.data = new Buffer.from(matches[2], 'base64');
+
+    var imageBuffer = response;
+    require('fs').writeFile(
+      'out.png',
+      imageBuffer['data'],
+      'binary',
+      function (err) {
+        if (err != null) {
+          console.log(err);
+        }
+      }
+    );
+  }
+
   async init({ session, headless, useChrome }) {
-    const bot = await create({
+    const bot = await create(
       session,
-      headless,
-      useChrome,
-      multidevice: false
-    });
+      this.fnLogQrCode,
+      undefined,
+      {
+        session,
+        headless,
+        useChrome,
+        multidevice: false,
+        logQR: false, 
+        disableWelcome: true
+      }
+      );
     this.#bots.push({session, bot});
 
     return bot;
